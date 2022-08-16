7372977ac59bb9811002f110a11ec75014c1692a
Rishita <ojharishita2529@gmail.com>
2022-08-16 15:22:22 +0530

    Added addition

--- hello.java
+++ hello.java
@@ -2,5 +2,7 @@ public class hello {
     public static void main() {
         System.out.print("Hello");
         System.out.print("Hello2");
+        int a=8,b=4;
+        System.out.println("Addition="+(a+b));
     }
 }
